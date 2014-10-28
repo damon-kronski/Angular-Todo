c42635c69cc6dc14649aad9f9dcdb964aafcb7bd
damon-kronski <suter.melvin@gmail.com>
2014-10-28 08:17:37 +0100

    Create deutsch.php

---
+++ language/deutsch.php
@@ -0,0 +1,20 @@
+<?php 
+	
+$language = array(
+	'PageTitle' => 'ToDo',
+	'Title' => 'Titel',
+	'Text' => 'Text',
+	'Description' => 'Beschreibung',
+	'Add' => 'Hinzufügen',
+	'Cancle' => 'Abbrechen',
+	'Save' => 'Speichern',
+	'Edit' => 'Bearbeiten',
+	'Delete' => 'Löschen',
+	'Yes' => 'Ja',
+	'No' => 'Nein',
+	'Login' => 'Login',
+	'Logout' => 'Logout',
+	'Password' => 'Passwort'
+);
+
+?>
