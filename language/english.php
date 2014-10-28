08e7b4e89c516d0db76f543735b647f326713340
damon-kronski <suter.melvin@gmail.com>
2014-10-28 08:17:50 +0100

    Create english.php

---
+++ language/english.php
@@ -0,0 +1,20 @@
+<?php 
+	
+$language = array(
+	'PageTitle' => 'ToDo',
+	'Title' => 'Title',
+	'Text' => 'Text',
+	'Description' => 'Description',
+	'Add' => 'Add',
+	'Cancle' => 'Cancle',
+	'Save' => 'Save',
+	'Edit' => 'Edit',
+	'Delete' => 'Delete',
+	'Yes' => 'Yes',
+	'No' => 'No',
+	'Login' => 'Login',
+	'Logout' => 'Logout',
+	'Password' => 'Password'
+);
+
+?>
