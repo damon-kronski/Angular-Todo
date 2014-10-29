a7426726f351cfa61956bbc9d067c1a175c344a2
damon-kronski <suter.melvin@gmail.com>
2014-10-29 14:25:31 +0100

    New JsonDB

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
 /***************************************************/
 
 // Base path
-$config['base_path'] = "http://localhost/angular.V3/";
+$config['base_path'] = "http://localhost/github/Angular-Todo/";
 	
 // Default Items for empty database
 $config['defaultArray'] = array(array('id' => 1,'title' => 'These is a sample todo','text' => 'With some description','date' => '01/01/2014'),array('id' => 2,'title' => 'And thats the other one','text' => '','date' => '01/01/2014'));
@@ -17,13 +17,13 @@ $config['defaultArray'] = array(array('id' => 1,'title' => 'These is a sample to
 /***************************************************/
 
 // Use Login?
-$config['useLogin'] = false;
+$config['useLogin'] = true;
 
 // SHA-256 encrypted password
 $config['loginKey'] = "937e8d5fbb48bd4949536cd65b8d35c426b80d2f830c5c308e2cdec422ae2244";
 
 // Store ToDo-List in Cookies? (else in json File on Server)
-$config['useCookies'] = true;
+$config['useCookies'] = false;
 	
 // Path to json file on server
 $config['jsonDBPath'] = "data/db.json";
--- lib/php/jsondb.php
+++ lib/php/jsondb.php
@@ -1,53 +1,94 @@
 <?php
 
-include("config/config.php");
+/****************************************************/
+/********************** JsonDB **********************/
+/****************************************************/
+/*                                                  */
+/*  Name:        JsonDB                             */
+/*  Author:      Damon-Kronski                      */
+/*  Version:     1.0                                */
+/*  Description:                                    */
+/*    A lightweight database library for            */
+/*    json files or cookies                         */
+/*                                                  */
+/****************************************************/
+
+
+
+class JsonDB{
+
+public $config = array();
+
+function __construct($useCookies = true, $jsonDBPath = "db.json",$cookieName = "JsonDB",$defaultArray = array(array('id' => 1,'name' => 'Test','value' => 'Value'))){
+	
+}
+
+    public static function withJson( $defaultArray = array(array('id' => 1,'name' => 'Test','value' => 'Value')), $jsonDBPath = "db.json" ) {
+    	$instance = new self();
+    	$instance->config['jsonDBPath'] = $jsonDBPath;
+    	$instance->config['defaultArray'] = $defaultArray;
+		$instance->config['useCookies'] = false;
+		$instance->config['cookieName'] = '';
+		
+		if(!file_exists($instance->config['jsonDBPath']))
+			$instance->writeDB($instance->config['defaultArray']);
+		
+		return $instance;
+    }
+
+    public static function withCookie( $defaultArray = array(array('id' => 1,'name' => 'Test','value' => 'Value')), $cookieName = "JsonDB" ) {
+    	$instance = new self();
+    	$instance->config['jsonDBPath'] = '';
+    	$instance->config['defaultArray'] = $defaultArray;
+		$instance->config['useCookies'] = true;
+		$instance->config['cookieName'] = $cookieName;
+		
+		if(!isset($_COOKIE[$cookieName]))
+			$instance->writeDB($instance->config['defaultArray']);
+		
+		return $instance;
+    }
+
+/***************************************************/
+/********************* Script **********************/
+/***************************************************/
 
 function delItem($id){
-	$db = readDB();
-	$dbnew = rmArray($db,$id);
-	writeDB($dbnew);
+	$db = $this->readDB();
+	$dbnew = $this->rmArray($db,$id);
+	$this->writeDB($dbnew);
 }
 
 function addItem($item){
-	$db = readDB();
-	$item['id'] = getNewId($db);
-	$dbnew = addArray($db,$item);
-	writeDB($dbnew);
+	$db = $this->readDB();
+	$item['id'] = $this->getNewId($db);
+	$dbnew = $this->addArray($db,$item);
+	$this->writeDB($dbnew);
 	return $item['id'];
 }
 
 function setItem($id,$value){
-	$db = readDB();
-	$dbnew = setArray($db,$id,$value);
-	writeDB($dbnew);
+	$db = $this->readDB();
+	$dbnew = $this->setArray($db,$id,$value);
+	$this->writeDB($dbnew);
 }
 
 function writeDB($array){
-	global $config;
-	if($config['useCookies'])
-		setcookie("TodoDB",json_encode($array));
+	if($this->config['useCookies'])
+		setcookie($this->config['cookieName'],json_encode($array));
 	else{
-		$fh = fopen($config['jsonDBPath'], 'w+');
+		$fh = fopen($this->config['jsonDBPath'], 'w+');
 		fwrite($fh, json_encode($array));
 		fclose($fh);
 	}
 }
 
 function readDB(){
-	global $config;
-	if($config['useCookies']){
-		if(isset($_COOKIE["TodoDB"]))
-			return json_decode($_COOKIE["TodoDB"], true);
-		else {
-			writeDB($config['defaultArray']);
-			return json_decode($config['defaultArray'], true);
-		}
-	}else{
-		if(!file_exists($config['jsonDBPath']))
-			writeDB($config['defaultArray']);
-			
-		$fh = fopen($config['jsonDBPath'], 'r');
-		$json = fread($fh, filesize("data/db.json"));
+	if($this->config['useCookies'])
+		return json_decode($_COOKIE[$this->config['cookieName']], true);
+	else{
+		$fh = fopen($this->config['jsonDBPath'], 'r');
+		$json = fread($fh, filesize($this->config['jsonDBPath']));
 		fclose($fh);
 		return json_decode($json, true);
 	}
@@ -93,6 +134,13 @@ function getNewId($db){
 	{
 		return 1;
 	}
+}
+
+function newId(){
+	$db = $this->readDB();
+	return getNewId($db);
+}
+
 }
 
 ?>
